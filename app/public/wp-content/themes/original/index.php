17ff3f29bb7810dfd3bcdeb716b493a0c4e48156
shodayuki <yshoda1227@gmail.com>
2020-08-15 18:18:53 +0900

    トップページ実装

--- app/public/wp-content/themes/original/index.php
+++ app/public/wp-content/themes/original/index.php
@@ -4,11 +4,36 @@
       <?php get_sidebar(); ?>
     </aside>
     <article>
-      <?php if(have_posts()): ?>
-      <?php else: ?>
-        <h2>投稿が見つかりません</h2>
-        <p><a href="<?php echo home_url(); ?>">トップページに戻る</a></p>
-      <?php endif; ?>
+    <section>
+      <h1>Academy Corporationのサイトへようこそ</h1>
+      <p>
+        Academy Corporationはあなたの生活を便利にする商品やサービスの開発を日々行っています。<br>
+        <a href="#">スタッフブログ</a>では新製品の情報なども公開していますので是非ご覧ください。
+      </p>
+      </section>
+      <section>
+        <h2 class="section-title">NEWS</h2>
+        <ul class="news-list">
+          <li>
+            <span class="date">2016.01.15</span>
+            <span class="label-info">お知らせ</span>
+            <a href="#">新製品「魔法の便利ハサミ」の発表イベントを開催します。</a>
+          </li>
+          <li>
+            <span class="date">2016.01.10</span>
+            <span class="label-info">お知らせ</span>
+            <a href="#">沢山のご要望により「らくらく便利シャープペンシル」の新カラーバリエーションを追加しました。</a>
+          </li>
+          <li>
+            <span class="date">2016.01.01</span>
+            <span class="label-info">お知らせ</span>
+            <a href="#">スタッフブログにて社員の年始ご挨拶を掲載しています。</a>
+          </li>
+        </ul>
+        <div class="center">
+          <a href="#" class="btn btn-default">お知らせ</a>
+        </div>
+      </section>
     </article>
   </div>
 <?php get_footer(); ?>
